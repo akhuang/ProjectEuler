8a0563d3b3171c99e2ca53b68e0ef5caf8a2a17e
phoenix <hf888.huang@gmail.com>
2014-03-11 22:33:24 +0800

    Add Problem 1

---
+++ src/Multiples_Of_3_And_5/Multiples_Of_3_And_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multiples_Of_3_And_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("please input a value:");
+                int maxValue = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(GetSum(maxValue));
+            }
+        }
+
+        static int GetSum(int maxValue)
+        {
+            int result = 0;
+            for (int i = 3; i < maxValue; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
